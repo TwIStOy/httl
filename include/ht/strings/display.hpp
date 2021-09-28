15601a3187aeaeb0b38cef238151183bc7fb5455
Hawtian Wang <twistoy.wang@gmail.com>
2021-09-28 14:33:28 +0800

    variant display impl

--- include/ht/strings/display.hpp
+++ include/ht/strings/display.hpp
@@ -10,6 +10,7 @@
 #include <ostream>
 #include <sstream>
 #include <string>
+#include <type_traits>
 
 namespace ht {
 
@@ -29,6 +30,32 @@ struct IDebugDisplay {
   inline std::string DebuggingStringify() const;
 };
 
+namespace display_helper {
+
+template<typename T>
+struct DisplayHelper {};
+
+template<typename T, typename = void>
+struct IsHelperImpled : public std::false_type {};
+
+template<typename T>
+struct IsHelperImpled<
+    T, std::void_t<decltype(DisplayHelper<T>{}(std::declval<T>()))>>
+    : std::true_type {};
+
+template<typename T>
+struct DebugDisplayHelper {};
+
+template<typename T, typename = void>
+struct IsDebugHelperImpled : public std::false_type {};
+
+template<typename T>
+struct IsDebugHelperImpled<
+    T, std::void_t<decltype(DebugDisplayHelper<T>{}(std::declval<T>()))>>
+    : std::true_type {};
+
+}  // namespace display_helper
+
 inline std::string IDisplay::Stringify() const {
   std::ostringstream oss;
   Stringify(oss);
@@ -38,6 +65,58 @@ inline std::string IDisplay::Stringify() const {
 inline std::string IDebugDisplay::DebuggingStringify() const {
   std::ostringstream oss;
   DebuggingStringify(oss);
+  return oss.str();
+}
+
+template<typename T,
+         typename = std::enable_if_t<std::is_base_of_v<IDisplay, T> ||
+                                     display_helper::IsHelperImpled<T>::value>>
+inline std::ostream &operator<<(std::ostream &oss, const T &obj) {
+  if constexpr (std::is_base_of_v<IDisplay, T>) {
+    static_cast<const IDisplay &>(obj).Stringify(oss);
+    return oss;
+  }
+  if constexpr (display_helper::IsHelperImpled<T>::value) {
+    display_helper::DisplayHelper<T> helper;
+    return oss << helper(obj);
+  }
+}
+
+template<typename T,
+         typename = std::enable_if_t<std::is_base_of_v<IDisplay, T> ||
+                                     display_helper::IsHelperImpled<T>::value>>
+void Stringify(std::ostream &oss, const T &obj) {
+  oss << obj;
+}
+
+template<typename T,
+         typename = std::enable_if_t<std::is_base_of_v<IDisplay, T> ||
+                                     display_helper::IsHelperImpled<T>::value>>
+std::string Stringify(const T &obj) {
+  std::ostringstream oss;
+  oss << obj;
+  return oss.str();
+}
+
+template<typename T, typename = std::enable_if_t<
+                         std::is_base_of_v<IDebugDisplay, T> ||
+                         display_helper::IsDebugHelperImpled<T>::value>>
+void DebugStringify(std::ostream &oss, const T &obj) {
+  if constexpr (std::is_base_of_v<IDebugDisplay, T>) {
+    static_cast<const IDebugDisplay &>(obj).DebuggingStringify(oss);
+  }
+  if constexpr (display_helper::IsDebugHelperImpled<T>::value) {
+    display_helper::DebugDisplayHelper<T> helper;
+    oss << helper(obj);
+  }
+}
+
+template<typename T, typename = std::enable_if_t<
+                         std::is_base_of_v<IDebugDisplay, T> ||
+                         display_helper::IsDebugHelperImpled<T>::value>>
+std::string DebugStringify(const T &obj) {
+  std::ostringstream oss;
+  DebugStringify(oss, obj);
   return oss.str();
 }
 
---
+++ include/ht/strings/display/variant.hpp
@@ -0,0 +1,77 @@
+// Template Library for C++
+//
+// Copyright (c) 2020 - present, Hawtian Wang
+// All rights reserved.
+//
+// For the license information refer to version.h.
+
+#pragma once  // NOLINT(build/header_guard)
+
+#include <cxxabi.h>
+
+#include <memory>
+#include <sstream>
+#include <string>
+#include <type_traits>
+#include <typeinfo>
+#include <variant>
+
+#include "ht/strings/display.hpp"
+
+namespace ht::display_helper {
+
+namespace __impl_variant {
+template<typename T>
+inline std::string Demangle() {
+  int status;
+  std::unique_ptr<char[], void (*)(void *)> result(
+      abi::__cxa_demangle(typeid(T).name(), 0, 0, &status), std::free);
+  return result.get() ? std::string(result.get()) : "???";
+}
+
+}  // namespace __impl_variant
+
+template<typename... Args>
+struct DisplayHelper<std::variant<Args...>> {
+  using variant_t = std::variant<Args...>;
+  std::string operator()(const variant_t &obj) const {
+    return std::visit(
+        [](const auto &r) {
+          return Stringify(r);
+        },
+        obj);
+  }
+};
+
+template<typename... Args>
+struct DebugDisplayHelper<std::variant<Args...>> {
+  using variant_t = std::variant<Args...>;
+
+  std::string operator()(const variant_t &obj) const {
+    std::ostringstream oss;
+    oss << "std::variant<";
+    MergeTypes<Args...>(oss);
+    oss << ">(";
+
+    std::visit(
+        [&](const auto &r) {
+          oss << __impl_variant::Demangle<std::decay_t<decltype(r)>>() << ": ";
+          DisplayDebugString(oss, r);
+        },
+        obj);
+    oss << ")";
+
+    return oss.str();
+  }
+
+ private:
+  template<typename First, typename... Rest>
+  static void MergeTypes(std::ostringstream &oss) {
+    oss << __impl_variant::Demangle<First>();
+    ((oss << ", " << __impl_variant::Demangle<Rest>()), ...);
+  }
+};
+
+}  // namespace ht::display_helper
+
+// vim: et sw=2 ts=2
