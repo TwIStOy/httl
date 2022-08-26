e1d4b6a1ba72f374d8cc183f0de9776ad0c71ef2
Hawtian Wang <twistoy.wang@gmail.com>
2022-08-27 00:06:30 +0800

    fix

---
+++ include/ht/parser_combinator/parser_combinator.hpp
@@ -0,0 +1,22 @@
+// Template Library for C++
+//
+// Copyright (c) 2020 - present, Hawtian Wang
+// All rights reserved.
+//
+// For the license information refer to version.h.
+
+#pragma once  // NOLINT(build/header_guard)
+
+#include "ht/parser_combinator/impl/commons/combinator_converter.hpp"
+#include "ht/parser_combinator/impl/commons/combinator_many.hpp"
+#include "ht/parser_combinator/impl/commons/combinator_option.hpp"
+#include "ht/parser_combinator/impl/commons/combinator_or.hpp"
+#include "ht/parser_combinator/impl/commons/combinator_plus.hpp"
+#include "ht/parser_combinator/impl/input_stream.hpp"
+#include "ht/parser_combinator/impl/parser.hpp"
+
+namespace ht::parser_combinator {
+
+using input_stream = _parser_combinator_impl::input_stream;
+
+}  // namespace ht::parser_combinator
