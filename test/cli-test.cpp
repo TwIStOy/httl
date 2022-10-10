576792f0b9ab03edec0a8b766e9d8da826ecda60
Hawtian Wang <twistoy.wang@gmail.com>
2022-10-10 11:26:00 +0800

    fix

--- test/cli-test.cpp
+++ test/cli-test.cpp
@@ -9,14 +9,14 @@
 #include <functional>
 
 #include "catch2/catch_all.hpp"
-#include "ht/core/hash.hpp"
+#include "ht/functional/hash.hpp"
 #include "ht/macro/stealer.h"
 
 using cli_parse_result =
     ht::result<ht::command_line_interpreter::input_command, std::string>;
 using cli_commands_type = std::unordered_map<
     std::string, std::unique_ptr<ht::command_line_interpreter::command_base>,
-    ht::string_hash, std::equal_to<void> >;
+    ht::Hash<std::string>, std::equal_to<void> >;
 
 STEALER(cli_interp_stealer, ht::command_line_interpreter,
         STEAL_METHOD(cli_parse_result, parse, const std::string&),
